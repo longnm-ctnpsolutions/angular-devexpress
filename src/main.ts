62819fe447146c72c4a0f6fa3becbddeba8620af
Long Nguyen <longnm@ctnpsolutions>
2024-12-25 14:47:41 +0700

    Init V3 - Angular Stadalone

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import themes from 'devextreme/ui/themes';
 
+
+themes.initialized(() => {
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+});
